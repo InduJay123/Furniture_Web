fe6bdb86a52a0108f101321d8dae4d6355fdd61a
Induni Jayamanna <152453002+InduJay123@users.noreply.github.com>
2026-01-15 16:38:25 +0530

    product update

--- src/components/shop/Products.jsx
+++ src/components/shop/Products.jsx
@@ -1,6 +1,11 @@
 const ProductsPage = () => {
     return (
-        <div>Products</div>
+        <div>
+            <h4> CURATED SELECTION</h4>
+            <h4> Featured Pieces</h4>
+            <h4> oUR COLLECTION </h4>
+            <h4> All Products</h4>
+        </div>
     )
 }
 
