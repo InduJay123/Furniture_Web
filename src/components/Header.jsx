a641e6c99baf61046469e9c09fc08e88bac8c018
Induni Jayamanna <152453002+InduJay123@users.noreply.github.com>
2026-01-15 16:24:20 +0530

    update 2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -20,7 +20,7 @@ const Header = () => {
           <h2 className="text-7xl sm:text-[6px] md:text-[82px] spin  right-[26%] top-[30%] inline-block max-w-3xl font-semibold">N</h2>
           <br/>
         <h3 className="text-md sm:text-1px md:text-[30px] inline-block max-w-4xl mt-6 text-white/70">| Every space has a story</h3>
-        <div className="space-x-6 mt-34">
+        <div className="space-x-6 mt-36">
             <a href="#Projects" className="border border-white px-8 py-3 rounded">Contact Us</a>
             <a href="#Contact" className="bg-green-800/90 px-8 py-3 rounded ">Shop Now</a>
         </div>
