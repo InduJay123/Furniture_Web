b0f19ced60d3c59a3d5118138bcf0d4403912920
Induni Jayamanna <152453002+InduJay123@users.noreply.github.com>
2026-01-16 21:23:12 +0530

    format change

--- src/components/shop/Products.jsx
+++ src/components/shop/Products.jsx
@@ -8,9 +8,9 @@ const ProductsPage = () => {
             <h4> OUR COLLECTION </h4>
             <h4> All Products</h4>
             <div className="min-h-screen py-20">
-      <h2 className="text-4xl font-semibold text-center mb-10">Our Products</h2>
-      <ProductList />
-    </div>
+                <h2 className="text-4xl font-semibold text-center mb-10">Our Products</h2>
+                <ProductList />
+            </div>
         </div>
     )
 }
