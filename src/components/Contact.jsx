daca74efb11ed8e6cd352256a746cb9ad639ccbf
Induni Jayamanna <152453002+InduJay123@users.noreply.github.com>
2026-01-16 22:32:29 +0530

    contact update 2

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -41,20 +41,20 @@ const Contact = () => {
       <h1 className="text-2xl sm:text-4xl font-bold mb-2 text-center">Contact <span className="underline underline-offset-4 decoration-1 under font-light">With Us</span></h1>
       <p className="text-center text-gray-500 mb-8 max-w-80 mx-auto">Ready to Make a Move? Lets Build Your Future Together</p>
 
-        <form onSubmit={onSubmit} className="max-w xl mx-52 text-gray-600 pt-8">
+        <form onSubmit={onSubmit} className="max-w xl mx-52 text-gray-600 pt-6">
             <div className="flex flex-wrap">
                 <div className="w-full md:w-1/2 text-left">
                     Your name
-                    <input className="w-full border border-gray-300 rounded py-3 px-4 mt-2" type="text" name="Name" placeholder="Your Name" required/>
+                    <input className="w-full border border-gray-300 rounded py-2 px-4 mt-2" type="text" name="Name" placeholder="Your Name" required/>
                 </div>
                 <div className="w-full md:w-1/2 text-left md:pl-4">
                     Your Email
-                    <input className="w-full border border-gray-300 rounded py-3 px-4 mt-2" type="email" name="Email" placeholder="Your Email" required/>
+                    <input className="w-full border border-gray-300 rounded py-2 px-4 mt-2" type="email" name="Email" placeholder="Your Email" required/>
                 </div>
             </div>
-            <div className="my-6 text-left">
+            <div className="my-4 text-left">
                 Message
-                <textarea  className="w-full border border-gray-300 rounded py-3 px-4 mt-2 h-48 resize-none" 
+                <textarea  className="w-full border border-gray-300 rounded py-2 px-4 mt-2 h-48 resize-none" 
                 name="Message" placeholder="Message" required></textarea>
             </div>
             <button className="bg-[#2B2622] text-white py-2 px-12 mb-10 rounded">{result ? result : "Send Message"}</button>
