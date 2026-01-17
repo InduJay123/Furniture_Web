631090c2e79ba66efb0d772557eadddbf277c910
Induni Jayamanna <152453002+InduJay123@users.noreply.github.com>
2026-01-17 09:32:21 +0530

    header update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -18,9 +18,9 @@ const Header = () => {
       whileInView={{opacity:1, y:0}}
       viewport={{once: true}}
       className="container relative z-10 text-center mx-auto py-4 mt-8 px-6 md:px-20 lg:px-32 text-white">
-          <h2 className="text-7xl sm:text-[8px] md:text-[90px] inline-block max-w-3xl font-semibold z-10">INTERIOR </h2><br/>
-          <h2 className="text-7xl sm:text-[6px] md:text-[90px] inline-block max-w-3xl font-semibold z-10">DESIG</h2>
-          <h2 className="text-7xl sm:text-[6px] md:text-[82px] spin  right-[26%] top-[30%] inline-block max-w-3xl font-semibold">N</h2>
+          <h2 className="text-7xl sm:text-[8px] md:text-[90px] inline-block max-w-3xl font-serif font-medium z-10">INTERIOR </h2><br/>
+          <h2 className="text-7xl sm:text-[6px] md:text-[90px] inline-block max-w-3xl font-serif font-medium z-10">DESIG</h2>
+          <h2 className="text-7xl sm:text-[6px] md:text-[82px] spin  right-[26%] top-[30%] inline-block max-w-3xl font-serif font-medium">N</h2>
           <br/>
         <h3 className="text-md sm:text-1px md:text-[30px] inline-block max-w-4xl mt-6 text-white/70">| Every space has a story</h3>
         <div className="space-x-6 mt-36">
