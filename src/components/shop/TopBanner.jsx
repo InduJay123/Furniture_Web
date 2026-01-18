cd37d5c6ae9eb9f0f1504be0fdbad20e464e1aa0
Induni Jayamanna <152453002+InduJay123@users.noreply.github.com>
2026-01-18 21:21:45 +0530

    style update

--- src/components/shop/TopBanner.jsx
+++ src/components/shop/TopBanner.jsx
@@ -1,6 +1,11 @@
+import livingRoom from "../assets/hero-living-room.jpg";
+
 const TopBanner = () => {
     return(
-        <div className="p-2"> 
+        <div className="h-40 p-2 bg-cover bg-center flex items-center justify-center"
+      style={{
+        backgroundImage: `url(${livingRoom})`,
+      }}> 
             <h1>Timeless Elegance</h1>
         </div>
     )
