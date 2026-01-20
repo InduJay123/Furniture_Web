0348c0190fd768cdf446f0d3ecbe34ba0a2f29a6
Induni Jayamanna <152453002+InduJay123@users.noreply.github.com>
2026-01-20 22:48:29 +0530

    tailwind update

--- src/components/shop/ShopNavbar.jsx
+++ src/components/shop/ShopNavbar.jsx
@@ -1,7 +1,9 @@
 const ShopNavbar = () => {
     return(
         <nav className="fixed top-0 left-0 w-full h-16 bg-white shadow z-50">
+            <div className="max-w-7xl mx-auto h-full px-6 flex items-center justify-between">
 
+            </div>
         </nav>
     )
 }
