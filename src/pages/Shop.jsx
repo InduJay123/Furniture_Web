203da60ca8bab327a18d255364b19cbd482c483b
Induni Jayamanna <152453002+InduJay123@users.noreply.github.com>
2026-01-20 22:51:47 +0530

    shop update

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -3,11 +3,13 @@ import Explore from "../components/shop/Explore";
 import FeaturePage from "../components/shop/FeatureProducts";
 import Newsletter from "../components/shop/Newsletter";
 import ProductsPage from "../components/shop/ProductsPage";
+import ShopNavbar from "../components/shop/ShopNavbar";
 import TopBanner from "../components/shop/TopBanner";
 
 const Shop = () => {
     return (
         <div>
+            <ShopNavbar/>
             <TopBanner/>
             <FeaturePage/>
             <Explore/>
