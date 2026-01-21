9d96bbe3dc64c6ce7869d6cb9236ef3c991e0e51
Induni Jayamanna <152453002+InduJay123@users.noreply.github.com>
2026-01-22 00:03:48 +0530

    product page api update

--- src/components/shop/ProductsPage.jsx
+++ src/components/shop/ProductsPage.jsx
@@ -1,15 +1,21 @@
 import ProductList from './ProductList'
+import PropTypes from "prop-types";
 
-const ProductsPage = () => {
+
+const ProductsPage = ({ onAddToCart }) => {
     return (
         <div className='py-4 px-16'>
             <div className="min-h-screen py-12 px-24">
                 <h6 className="text-sm text-gray-400 tracking-widest mb-4"> OUR COLLECTION </h6>            
                 <h2 className="text-5xl font-serif font-medium text-black/80 mb-10">All Products</h2>
-                <ProductList />               
+                <ProductList onAddToCart={onAddToCart}/>               
             </div>
         </div>
     )
 }
+
+ProductsPage.propTypes = {
+  onAddToCart: PropTypes.func.isRequired,
+};
 
 export default ProductsPage
