3ee7a5d48608634b356578e5175e7519dabf900b
Induni Jayamanna <152453002+InduJay123@users.noreply.github.com>
2026-03-15 12:05:32 +0530

    axios update

--- furniture_web/src/user/components/Banner.jsx
+++ furniture_web/src/user/components/Banner.jsx
@@ -4,7 +4,7 @@ const Banner = () => {
   return (
     <div className="py-2">
      <div className="h-96 mb-2 bg-cover bg-center flex items-center w-full h-10 overflow-hidden" 
-    style={{backgroundImage: "url('/img5.avif')"}}>
+    style={{backgroundImage: `url(${assets.img5})`}}>
       <div className="grid grid-cols-1 sm:grid-cols-2 items-center gap-8 ml-24">
         <div className="space-y-2 max-w-xl mx-auto">
           <h1 className="text-2xl text-center sm:text-left sm:text-4xl font-semibold pl-24 mb-10 text-white/90">Modi Furn is available for Android and IOS</h1>
